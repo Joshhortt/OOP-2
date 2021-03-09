1365e962f4ba81c2b948078e280ca8a1af22ee46
Joshhortt <joshhortt@yahoo.com>
2021-03-09 19:52:51 +0000

    Add method 'Purchase' to class 'BookModel' that checks quantity in stock, with Inheritance of Interface 'IPurchasable'.

--- 03-MiniProject/InheritanceMiniProject/Models/BookModel.cs
+++ 03-MiniProject/InheritanceMiniProject/Models/BookModel.cs
@@ -2,8 +2,16 @@
 
 namespace InheritanceMiniProject
 {
-    public class BookModel : InventoryItemModel  // 3. Add inheritance
-    {
-        public int NumberOfPages { get; set; }  // 2. Add property
-    }
+    public class BookModel : InventoryItemModel, IPurchasable // 3. Add inheritance,
+											   // 24.  Add Interface IPurchasable + CTRL + DOT to implement Interface 'Purchase'.
+	{
+		public int NumberOfPages { get; set; }  // 2. Add property
+
+		public void Purchase()   // 25. Add method created from IPurchasable 24.
+		{
+			//throw new NotImplementedException();  26. Remove or comment
+			QuantityInStock -= 1;  // 27. check quantity
+			Console.WriteLine("This book has been purchased.");  // 28. add message
+		}
+	}
 }
