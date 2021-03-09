f1f229f0c21299f7b4db8759237fb2ab34fea2d7
Joshhortt <joshhortt@yahoo.com>
2021-03-09 19:44:49 +0000

    Add methods 'Rent' & 'ReturnRental' to class 'ExcavatorModel' that checks quantity in stock, with Inheritance of Interface 'IRentable'.

--- 03-MiniProject/InheritanceMiniProject/Models/ExcavatorModel.cs
+++ 03-MiniProject/InheritanceMiniProject/Models/ExcavatorModel.cs
@@ -2,11 +2,25 @@
 
 namespace InheritanceMiniProject
 {
-    public class ExcavatorModel : InventoryItemModel  // 10. Inheritance
+    public class ExcavatorModel : InventoryItemModel, IRentable  // 10. Inheritance ,  
+													// 18. Add Interface IRentable + CTRL + DOT to implement Interface 'ReturnRental'
     {
        public void Dig()  // 09. Method
 		{
 			Console.WriteLine("I am Digging!");  // 11.  message
 		}
-    }
+
+        public void Rent()  // 16. Add method
+		{
+			QuantityInStock -= 1;  // 20. check quantity
+			Console.WriteLine("This Excavator has been rented.");  // 17. add message
+		}
+
+		public void ReturnRental()  // 19. Add method created from IRentable 18.
+		{
+			//throw new NotImplementedException();  21. Remove or comment
+			QuantityInStock += 1;  // 22. check quantity
+			Console.WriteLine("This Excavator has been returned.");  // 23. add message
+		}
+	}
 }
--- 03-MiniProject/InheritanceMiniProject/Models/InventoryItemModel.cs
+++ 03-MiniProject/InheritanceMiniProject/Models/InventoryItemModel.cs
@@ -3,6 +3,6 @@
     public class InventoryItemModel
     {
         public string ProductName { get; set; }  // 1. Add property
-        public string QuantityInStock { get; set; }  // 12. Add property
+        public int QuantityInStock { get; set; }  // 12. Add property
     }
 }
