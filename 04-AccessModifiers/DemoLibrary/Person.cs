812a1defb93af8ed797ec383f626fd992171b299
Joshhortt <joshhortt@yahoo.com>
2021-03-11 10:01:46 +0000

    Fc. Add project 04 about 'Access Modifiers'. Add class Library with some sample public and private access modifiers.

---
+++ 04-AccessModifiers/DemoLibrary/Person.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLibrary
+{
+	public class Person
+	{
+		public string FirstName { get; set; }  // 1.
+
+		private string _ssn;  // 5.
+
+		public string SSN   // 4.
+		{
+			get
+			{
+				return $"***-**--{_ssn.Substring(_ssn.Length - 4) }"; // 6.
+			}
+			set
+			{
+				_ssn = value; // 7. 
+			}
+		}
+
+		public void SayHello()  // 2.
+		{
+			Console.WriteLine($"Hello { FirstName }");  // 3.
+		}
+	}
+}
