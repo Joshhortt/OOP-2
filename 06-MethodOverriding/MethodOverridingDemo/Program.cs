94e8aa39472f98511c13bfb14ff0c3c112262ddd
Joshhortt <joshhortt@yahoo.com>
2021-03-11 16:23:31 +0000

    Create afew more classses using 'virtual' method and the 'override' method.

--- 06-MethodOverriding/MethodOverridingDemo/Program.cs
+++ 06-MethodOverriding/MethodOverridingDemo/Program.cs
@@ -24,4 +24,34 @@ namespace MethodOverridingDemo
 			Console.ReadLine();  // 1. 
 		}
 	}
+	public abstract class Car // 23. Abstract class
+	{
+		public virtual void StartCar()  // 24. virtual
+		{
+			Console.WriteLine("Turn on key and start");  // 25.
+		}
+		// internal abstract void SetClock();  // 26. internal abstract is possible but if you would use Interface, it wouldn't be possible because
+												// Interface averything is public.
+		public abstract void SetClock();  // 27. 
+	}
+	public class Corolla : Car  // 28. 
+	{
+		//internal override void SetClock()  29. -- 26
+		public override void SetClock()  // 30
+		{
+			Console.WriteLine("Fiddle with the Corolla Clock.");  // 31.
+		}
+	}
+	public class Tesla : Car  // 32. 
+	{
+		public override void StartCar()  // 35. override
+		{
+			Console.WriteLine("Think about your destination.");  // 36.
+		}
+
+		public override void SetClock()  // 33.
+		{
+			Console.WriteLine("It sets itself.");  // 34.
+		}
+	}
 }
