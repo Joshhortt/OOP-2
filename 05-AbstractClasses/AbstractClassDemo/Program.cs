1e19b53b7fa9c525ab774395f2fde3871eae6daf
Joshhortt <joshhortt@yahoo.com>
2021-03-11 15:05:30 +0000

    Removed step by step code development comments from 'Program.cs'.

--- 05-AbstractClasses/AbstractClassDemo/Program.cs
+++ 05-AbstractClasses/AbstractClassDemo/Program.cs
@@ -12,51 +12,49 @@ namespace AbstractClassDemo
 	{
 		static void Main(string[] args)
 		{
-			Car car = new Car();  // 9. 
-			car.NumberOfWheels = 4;  // 10. 
+			Car car = new Car();  
+			car.NumberOfWheels = 4;   
 			car.Manufacturer = "Honda";
-			Console.ReadLine();  // 1. 
-
+			Console.ReadLine();  
 		}
 	}
 
-	public abstract class Vehicle  // 2. A vehicle is abstract so you can't instantiate it directly, you have to inherit them from another class..
+	public abstract class Vehicle 
 		{
-			public string VIN { get; set; } // 3.
-			public string Manufacturer { get; set; }  // 4.
-			public string Model { get; set; } // 5.
-			public int Year { get; set; } // 6.
+			public string VIN { get; set; } 
+			public string Manufacturer { get; set; } 
+			public string Model { get; set; } 
+			public int Year { get; set; }
 	}
-	public class Car : Vehicle  // 7.
+	public class Car : Vehicle 
 	{
-		public int NumberOfWheels { get; set; } = 4;  // 8.
+		public int NumberOfWheels { get; set; } = 4; 
 	}
 }
 
 #region First Example
 
 /*
-InventoryItem item = new Book();  // 7. You can instantiate like this
-item.ProductName = "A Tale of Two Cities";  // 8.   "
+InventoryItem item = new Book();  
+item.ProductName = "A Tale of Two Cities"; 
 
-//InventoryItem item = new InventoryItem();  // 9. cannot create an instance of the abstract InventoryItem
+//InventoryItem item = new InventoryItem();  
 
 Console.ReadLine();  // 1.
 }
 }
 
-public abstract class InventoryItem  // 2. 'abstract class' it's a base class. It's a class that you build upon but it's not very complete by it's own.
-{
-public string ProductName { get; set; }  // 3.
-public int QuantityOnHand { get; set; }  // 4.
+public abstract class InventoryItem  
+public string ProductName { get; set; }  
+public int QuantityOnHand { get; set; } 
 }
 
-public class Book : InventoryItem  // 5.
+public class Book : InventoryItem .
 {
 
 }
 
-public class Vehicle : InventoryItem  // 6.
+public class Vehicle : InventoryItem 
 {
 
 }
