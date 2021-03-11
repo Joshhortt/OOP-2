b51b1bc72e0ac7204640291b3fdf8473f49aa0ab
Joshhortt <joshhortt@yahoo.com>
2021-03-11 15:27:31 +0000

    Fc. Add project 06 about 'Method Overriding'. Add a new class, with 3 properties and a new instance.

---
+++ 06-MethodOverriding/MethodOverridingDemo/Program.cs
@@ -0,0 +1,34 @@
+﻿// Object Oriented Programming - part 2 - Method Overriding
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverridingDemo
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			PersonModel person = new PersonModel
+			{
+				FirstName = "Josh",
+				LastName = "Hortt",
+				Email = "joshhortt@yahoo.com"
+		};
+
+		Console.WriteLine(person);
+
+
+			Console.ReadLine();
+		}
+	}
+	public class PersonModel
+	{
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public string Email{ get; set; }
+	}
+}
