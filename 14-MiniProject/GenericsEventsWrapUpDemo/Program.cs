a7b20c99b2efc62672053612903130465743c21a
Joshhortt <joshhortt@yahoo.com>
2021-03-17 18:07:14 +0000

    Add Mini-Project 14 'GenricsEventsWrapUp'. Add class 'PersonModel' with props and add class 'CarModel' with props.

---
+++ 14-MiniProject/GenericsEventsWrapUpDemo/Program.cs
@@ -0,0 +1,31 @@
+﻿// Object Oriented Programming - part 2 - Mini-Project - Generics & Events
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsEventsWrapUpDemo
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.ReadLine();
+
+		}
+	}
+
+	public class PersonModel
+	{
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public string Email { get; set; }
+	}
+
+	public class CarModel
+	{
+		public string Manufacturer { get; set; }
+		public string Model { get; set; }
+	}
+}
