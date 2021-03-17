dd45f3e635c1c23c56101551699a58439021b480
Joshhortt <joshhortt@yahoo.com>
2021-03-17 16:38:17 +0000

    Add project 13 about 'Events'.

---
+++ 13-Events/EventsDemo/Program.cs
@@ -0,0 +1,50 @@
+﻿// Object Oriented Programming - part 2 - Events
+using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SchoolClass history = new SchoolClass("History 101", 4);
+            SchoolClass english = new SchoolClass("Public Speaking 302", 3);
+
+			Console.WriteLine("\nSchool Enrollment Report:");
+			Console.WriteLine("--------------------------------------\n");
+
+            history.EnrollmentFull += SchoolClass_EnrollmentFull;
+
+            history.SignUpStudent("Josh Hortt").PrintToConsole();
+            history.SignUpStudent("Ana Calvario").PrintToConsole();
+            history.SignUpStudent("Alex Rebelo").PrintToConsole();
+            history.SignUpStudent("Sofia Nasala").PrintToConsole();
+            history.SignUpStudent("Bruno Vieira").PrintToConsole();
+            history.SignUpStudent("Pedro Guerreiro").PrintToConsole();
+            history.SignUpStudent("Mara Horta").PrintToConsole();
+
+            Console.WriteLine();
+
+            english.EnrollmentFull += SchoolClass_EnrollmentFull;
+
+            english.SignUpStudent("Alex Rebelo").PrintToConsole();
+            english.SignUpStudent("Sofia Nasala").PrintToConsole();
+            english.SignUpStudent("Bruno Vieira").PrintToConsole();
+            english.SignUpStudent("Mara Horta").PrintToConsole();
+
+            Console.ReadLine();
+        }
+
+        private static void SchoolClass_EnrollmentFull(object sender, string e)
+        {
+            SchoolClass model = (SchoolClass)sender;
+
+            Console.WriteLine();
+            Console.WriteLine($"{ model.CourseTitle }: Full");
+            Console.WriteLine();
+        }
+    }
+}
---
+++ 13-Events/EventsDemo/SchoolClass.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EventsDemo
+{
+    public class SchoolClass
+    {
+        public event EventHandler<string> EnrollmentFull;
+
+        private readonly List<string> enrolledStudents = new List<string>();
+        private readonly List<string> waitingList = new List<string>();
+
+        public string CourseTitle { get; private set; }
+        public int MaximumStudents { get; private set; }
+
+        public SchoolClass(string title, int maximumStudents)
+        {
+            CourseTitle = title;
+            MaximumStudents = maximumStudents;
+        }
+
+        public string SignUpStudent(string studentName)
+        {
+            string output = "";
+
+            if (enrolledStudents.Count < MaximumStudents)
+            {
+                enrolledStudents.Add(studentName);
+                output = $"{ studentName } was enrolled in { CourseTitle }";
+
+                // Check to see if reached maximum
+                if (enrolledStudents.Count == MaximumStudents)
+                {
+                    EnrollmentFull?.Invoke(this, $"{ CourseTitle } enrollment is now full.");
+                }
+            }
+            else
+            {
+                waitingList.Add(studentName);
+                output = $"{ studentName } was added to the wait list in { CourseTitle }";
+            }
+
+            return output;
+        }
+    }
+}
