1f0e058758a2622020239fb84dd32654a24e9f87
Joshhortt <joshhortt@yahoo.com>
2021-03-21 16:26:07 +0000

    Add OOP Final Project using Console Application.

---
+++ 15-FinalProject/FastMovingTrafficApp/FastMovingTraffic/Highway.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FastMovingTraffic
+{
+	class Highway
+	{
+		// properties
+		public string Name { get; set; }
+		public string HighwayType{ get; set; }
+		public char Direction { get; set; }
+		public string Surface { get; set; }
+		public int NumbLanes { get; set; }
+		public bool Toll { get; set; }
+		public string Maintainance { get; set; }
+
+		// constructors
+
+		public Highway(string name, string type, char direction, string surface, int numbLanes, bool toll, string maintainance)
+		{
+			Name = name;
+			HighwayType = type;
+			Direction = direction;
+			Surface = surface;
+			NumbLanes = numbLanes;
+			Toll = toll;
+			Maintainance = maintainance;
+		}
+
+		public Highway(string name)
+		{
+			Name = Name;
+		}
+
+		public Highway(string name, bool toll)
+		{
+			Name = name;
+			Toll = toll;
+		}
+
+		public Highway(string name, int numbLanes)
+		{
+			Name = name;
+			NumbLanes = numbLanes;
+		}
+
+		// Tostring method that return everything
+
+		public override string ToString()
+		{
+			//return base.ToString();
+			return $"Highway Name: {Name}\n" +
+				   $"Highway Type: {HighwayType}\n" +
+				   $"Direction: {Direction}\n" +
+				   $"Surface: {Surface}\n" +
+				   $"Number of Lanes: {NumbLanes}\n" +
+				   $"Toll: {Toll}\n" +
+				   $"Maintainance: {Maintainance}\n";
+		}
+	}
+}
---
+++ 15-FinalProject/FastMovingTrafficApp/FastMovingTraffic/Program.cs
@@ -0,0 +1,17 @@
+﻿// Object Oriented Programming - part 2 - Final-Project 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FastMovingTraffic
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.ReadLine();
+		}
+	}
+}
